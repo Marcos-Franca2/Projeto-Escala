b00f23286d4a8c6beb19236718c57d21af250845
Marcos França <marcosfranca.vani@gmail.com>
2022-07-21 20:33:46 -0300

    remoção de obejetos ja escalados do local storage

--- js/escala.js
+++ js/escala.js
@@ -6,11 +6,48 @@ function listarMotoristasEscalas() {
     listaMotorista.forEach(element => {
         htmlLista += `
         <div class = "container">
-            <input type="checkbox" id=${element.matricula} name=${element.matricula}>
+            <input type="checkbox" class = "teste" id=${element.matricula} name=${element.nomeMotorista}>
             <div class = "linha" ><b>Matricula: </b>${element.matricula} </div>
             <div class = "namemot"><b>Nome: </b>${element.nomeMotorista} </div>
         </div>
         ` 
     }); 
     document.getElementById('listaMotorista').innerHTML = htmlLista
+    console.log(listaMotorista)
+
+}
+function escalar(){
+    let radioselect = document.querySelectorAll(`input[class = "radio"]:checked`);
+    let radiobox = []
+    radioselect.forEach((checkbox) => {
+            radiobox.push(checkbox.value);
+    }); 
+   
+    let checkboxes = document.querySelectorAll(`input[class = "teste"]:checked`);
+    let moto = []
+    checkboxes.forEach((checkbox) => {
+            moto.push(checkbox.id + ` ` + checkbox.name);
+    });
+
+    var horarioescalado = [(String(radiobox) + ' = ' + String(moto) + '<br>')]
+
+    document.getElementById('horarioe').innerHTML += horarioescalado 
+
+    let idselect = [] 
+    checkboxes.forEach((checkbox) => {
+        idselect.push(checkbox.id);
+        });
+        var idclean = idselect.pop()
+        console.log(idclean);
+
+        var index = listaMotorista.map(x => {
+            return x.matricula;
+          }).indexOf(idclean);
+          console.log(index);
+          listaMotorista.splice(index, 1);
+          console.log(listaMotorista);
+
+ listarMotoristasEscalas()
+  //  var time = document.querySelector('.radio:checked').value;
+        
 }
--- js/geral.js
+++ js/geral.js
@@ -9,3 +9,4 @@ else {
     listaMotorista = JSON.parse(listaMotorista)
 }
 
+
